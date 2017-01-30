4373eff619e33a8512f19bb8c67be05123a81891
Sebastian Wieland <swieland@conexco.com>
2017-01-30 10:54:48 +0100

    Added 'stream' post parameter to nowplaying.sh

--- .sh/nowplaying.sh
+++ .sh/nowplaying.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 nowplaying() {
-	local egofmpage="`wget -qO- 'http://www.egofm.de/musik/play-history'`"
+	local stream='egoFM'
+
+	if [ "$#" -eq 1 ]; then
+		stream=$1
+	fi
+
+    local egofmpage="`wget -qO- 'http://www.egofm.de/musik/play-history' --post-data "stream=$stream"`"
 	[[ $egofmpage =~ '<div class="artist">'([a-zA-Z. 0-9&;-]*) ]] && echo "Artist:" ${BASH_REMATCH[1]}
 	[[ $egofmpage =~ '<div class="name">&nbsp;'([a-zA-Z. 0-9&;-]*) ]] && echo "Name:" ${BASH_REMATCH[1]}
 }
