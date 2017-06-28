d35e6812cea798f6f406c0e257b9c7f0ee47650b
Sebastian Wieland <swieland@conexco.com>
2017-06-28 10:16:16 +0200

    Minor changes to tmp garbage collector

--- .sh/delete_old_tmp_files.sh
+++ .sh/delete_old_tmp_files.sh
@@ -3,6 +3,10 @@
 delete_old_tmp_files() {
 		local currdir="$(pwd)"
 
+		cd ~/.Trash
+		find . -type f -mtime +1 -delete
+		find . -mindepth 1 -type d -empty -delete
+
 		cd ~/tmp
 		find . -type f -mtime +7 -delete
 		find . -mindepth 1 -type d -empty -delete
@@ -12,4 +16,6 @@ delete_old_tmp_files() {
 		find . -mindepth 1 -type d -empty -delete
 
 		cd $currdir
+
+		osascript -e 'display notification "Deleted old tmp files" with title "Tmp Garbage Collector"'
 }
